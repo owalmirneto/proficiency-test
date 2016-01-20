5d0e484ea388bda76097086a16545540358b4fb8
Walmir Neto <wfsneto@gmail.com>
2016-01-20 20:53:01 -0300

    adjustments on dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @courses = Course.limit(5).order(created_at: :desc).decorate
+    @classrooms = Classroom.limit(5).order(created_at: :desc).decorate
   end
 end
