7840cdce5a47846a806f29b5959c01a074ef4d11
Walmir Neto <wfsneto@gmail.com>
2016-01-20 18:57:03 -0300

    adjustments course tests

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  # it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(45) }
-  # it { is_expected.to validate_length_of(:description).is_at_least(2).is_at_most(45) }
   it { should ensure_inclusion_of(:status).in_array(CourseStatus.list) }
+
+  let(:course) { Course.new(name: "Walmir Neto", description: 'ABC1234', status: CourseStatus::OPEN) }
+
+  describe 'when name is too long' do
+    before { course.name = 'ab123' * 10 }
+    it do
+      course.valid?
+      expect(course.errors).to have_key(:name)
+    end
+  end
+
+  describe 'when description is too long' do
+    before { course.description = '1234' * 12 }
+    it do
+      course.valid?
+      expect(course.errors).to have_key(:description)
+    end
+  end
 end
