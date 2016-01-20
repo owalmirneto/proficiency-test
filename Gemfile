c296c62f429b399eb80db02eb4f6f95ea5d023ec
Walmir Neto <wfsneto@gmail.com>
2016-01-20 14:47:43 -0300

    Create validations for students

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'coffee-rails', '~> 4.0.0'
 
 gem 'jquery-rails'
 gem 'turbolinks'
-gem 'jbuilder', '~> 1.2'
+gem 'jbuilder', '~> 2.0'
 gem 'therubyracer', platforms: :ruby
 
 gem 'simple_form', '~> 3.0.2'
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 2 , maximum: 45}
+  validates :register_number, numericality: { only_integer: true }, presence: true, uniqueness: true
+  validates :status, presence: true
 end
