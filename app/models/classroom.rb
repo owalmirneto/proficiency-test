ef5427656e55c47ca826c03d02516f93116ba1c3
Walmir Neto <wfsneto@gmail.com>
2016-01-20 23:21:26 -0300

    Classroom tests fixed

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,9 +2,9 @@ class Classroom < ActiveRecord::Base
   belongs_to :student
   belongs_to :course
 
-  validates :student, presence: true,
+  validates :student_id, presence: true,
     uniqueness: { scope: :course, message: 'já está inscrito nesse curso' },
     inclusion: { in: StudentStatus.values_for(%w(ACTIVE)), message: "está inativo" }
-  validates :course, presence: true,
+  validates :course_id, presence: true,
     inclusion: { in: CourseStatus.values_for(%w(WAIT OPEN)), message: "está fechado" }
 end
--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -2,5 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Classroom, type: :model do
   it { should belong_to(:student) }
+  it { is_expected.to validate_presence_of(:student_id) }
+  it { should ensure_inclusion_of(:student_id).in_array(StudentStatus.values_for(%w(ACTIVE))) }
+
   it { should belong_to(:course) }
+  it { is_expected.to validate_presence_of(:course_id) }
+  it { should ensure_inclusion_of(:course_id).in_array(CourseStatus.values_for(%w(WAIT OPEN))) }
+
+  # it { is_expected.to validate_uniqueness_of(:student_id).scoped_to(:course_id) }
+  let(:student) { Student.create(name: "Walmir Neto", register_number: 'ABC1234', status: StudentStatus::ACTIVE) }
+  let(:course) { Course.create(name: "Frevo", description: 'on Rails', status: CourseStatus::OPEN) }
+  let(:classroom) { Classroom.new(student: student, course: course) }
+
+  describe 'when student is already enrolled at course' do
+    before { Classroom.create(student: student, course: course) }
+    it do
+      classroom.valid?
+      expect(classroom.errors).to have_key(:student_id)
+    end
+  end
 end
